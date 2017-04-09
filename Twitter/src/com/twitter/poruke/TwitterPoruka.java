aa98e35010e4d21febd5c7b27cf33154f914c469
UrosMomcilovic <urosuki968@gmail.com>
2017-04-09 18:04:26 +0200

    Napisana dokumentacija za klasu TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,31 +1,74 @@
 package com.twitter.poruke;
 
+/**
+ * 
+ * Klasa TwitterPoruka predstavlja poruku nekog korisnika
+ * na twitteru.
+ * Ima nadimak korisnika i poruku korisnika.
+ * 
+ * @author Uros Momcilovic
+ * @version 1.0
+ *
+ */
+
 public class TwitterPoruka {
 	
+	/**
+	 * Nadimak korisnika kao String
+	 */
 	private String korisnik;
+	
+	/**
+	 * Sadrzaj poruke kao String
+	 */
 	private String poruka;
 	
+	/**
+	 * Vraca nadimak korisnika
+	 * @return nadimak korisnika kao String
+	 */
 	public String getKorisnik() {
-	return korisnik;
-	}
-	public void setKorisnik(String korisnik) {
-	if (korisnik==null || !korisnik.isEmpty())
-	throw new RuntimeException(
-	"Ime korisnika mora biti uneto");
-	this.korisnik = korisnik;
-	}
-	public String getPoruka() {
-	return "poruka";
-	}
-	public void setPoruka(String poruka) {
-	if (this.poruka==null || this.poruka == new String("") ||
-	this.poruka.length()>140)
-	throw new RuntimeException(
-	"Poruka mora biti uneta i mora imati najvise 140 znakova");
-	this.poruka = poruka;
+		return korisnik;
 	}
 	
+	/**
+	 * Postavalja novi nadimak korisnika
+	 * @param korisnik novi nadimak korisnika
+	 * @throws java.lang.RuntimeException Ako je unet nadimak null
+	 */
+	public void setKorisnik(String korisnik) {
+		if (korisnik==null || !korisnik.isEmpty())
+			throw new RuntimeException("Ime korisnika mora biti uneto");
+		this.korisnik = korisnik;
+	}
+	
+	/**
+	 * Vraca sadrzaj poruke korisnika
+	 * @return sadrzaj poruke kao String
+	 */
+	public String getPoruka() {
+		return "poruka";
+	}
+	
+	/**
+	 * Pstavlja novu poruku korisnika
+	 * @param poruka sadrzaj nove poruke
+	 * @throws java.lang.RuntimeException Ako je poruka null 
+	 * ili ako je poruka String kraci od 140
+	 */
+	public void setPoruka(String poruka) {
+		if (this.poruka==null || this.poruka == new String("") ||
+				this.poruka.length()>140)
+			throw new RuntimeException(
+	"Poruka mora biti uneta i mora imati najvise 140 znakova");
+		this.poruka = poruka;
+	}
+	
+	/**
+	 * Vraca nadimak korisnika i njegovu poruku
+	 * @return nadimak korisnika i njegova poruka kao String
+	 */
 	public String toString(){
-	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
+		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	}
 }
