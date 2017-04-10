21d2f025fa15ffff7441f4ff74759f618cd582ab
UrosMomcilovic <urosuki968@gmail.com>
2017-04-10 22:19:24 +0200

    Ispravka greske u prethodnom commitu(nije commitovana klasa TwiterPorukaTest), kreirana klasa Twitter

---
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.twitter.poruke;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwitterPorukaTest {
+	
+	private TwitterPoruka t;
+
+
+	@Before
+	public void setUp() throws Exception {
+		t = new TwitterPoruka();
+	}
+
+	
+	@After
+	public void tearDown() throws Exception {
+		t = null;
+	}
+
+
+	@Test
+	public void testSetKorisnik() {
+		t.setKorisnik("King");
+		
+		assertEquals("King", t.getKorisnik());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetKorisnikNull() {
+		
+		t.setKorisnik(null);
+		
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetKorisnikEmpty() {
+		
+		t.setKorisnik("");
+		
+	}
+
+	
+	@Test
+	public void testSetPoruka() {
+		t.setPoruka("pozdrav");
+		
+		assertEquals("pozdrav", t.getPoruka());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaNull() {
+		
+		t.setPoruka(null);
+		
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPorukaEmpty() {
+		
+		t.setPoruka("");
+		
+	}
+
+	
+	@Test
+	public void testToString() {
+		t.setKorisnik("King");
+		t.setPoruka("pozdrav");
+		
+		assertEquals("KORISNIK:"+t.getKorisnik()+" PORUKA:"+t.getPoruka(), t.toString());
+		
+	}
+
+}
